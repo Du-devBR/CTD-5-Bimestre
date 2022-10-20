dce96cc374fcbff12e7243508daccbefd7ca37a0
Du-devBR <eduardo.ananiasdev@outlook.com>
2022-10-19 22:46:10 -0300

    aula 2

---
+++ Frontend-III/aula01-iniciandoReact/aula01-iniciandoReact/src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,16 @@
+import './style.scss'
+
+export function SegundaAula(){
+  return(
+    <main>
+      <h1>Segunda Aula</h1>
+      <h2>Projeto escolhido: Globo esporte</h2>
+      <ul>
+        <li>Header</li>
+        <li>Cards</li>
+        <li>Tabela</li>
+        <li>Shorts</li>
+      </ul>
+    </main>
+  )
+}
---
+++ Frontend-III/aula01-iniciandoReact/aula01-iniciandoReact/src/pages/Login/index.jsx
@@ -0,0 +1,7 @@
+import "./style.scss"
+
+export function LoginPage(){
+  return(
+    <h1 className="">Componente teste</h1>
+  )
+}
